ef0f79daeacffbf67b1c4a293392e8cfc6f12200
Đàm Tiến Đạt <tdat0306@gmail.com>
2025-04-17 14:35:29 +0000

    Add tasks

---
+++ backend/app/utils/model.py
@@ -0,0 +1,21 @@
+
+
+class Model:
+  def __init__(self):
+    pass
+
+  def preprocess(img):
+    pass
+
+  def postprocess(model_result):
+    pass
+
+  def get_overlaid_mask(img, binary_mask):
+    pass
+
+  def cdf(diameters):
+    pass
+
+  def predict(img):
+    pass
+
