4d236ef414508f516940c8588ebda399b51697b6
Đàm Tiến Đạt <tdat0306@gmail.com>
2025-04-18 14:09:58 +0000

    Update task: Store cdf data in database

--- backend/app/models/queries.py
+++ backend/app/models/queries.py
@@ -2,6 +2,7 @@ from sqlmodel import Session
 from app.models.database import User, Image
 from fastapi import HTTPException
 import uuid
+import numpy as np
 
 def create_user(session: Session, username: str, hashed_password: str) -> User:
   pass
@@ -9,7 +10,7 @@ def create_user(session: Session, username: str, hashed_password: str) -> User:
 def get_user_by_username(session: Session, username: str) -> User:
   pass
 
-def store_image_metadata(session: Session, user_id: uuid.UUID, bucket_name: str, minio_object_key: str):
+def store_image_metadata(session: Session, user_id: uuid.UUID, bucket_name: str, minio_object_key: str, cdf_data: np.ndarray):
   pass
 
 def get_all_image_metadata_by_user(session: Session, user_id: uuid.UUID) -> list[dict]:
--- backend/app/utils/minio.py
+++ backend/app/utils/minio.py
@@ -9,6 +9,9 @@ class MinIOClient:
   def upload_image(self, user_id: uuid.UUID, image_data: bytes, image_id: uuid.UUID) -> str:
     pass
 
+  def get_presigned_url(self, object_key: str) -> str:
+    pass
+
   def get_images_from_metadata(self, metadata_list: list[dict]) -> list[Image]:
     pass
 
