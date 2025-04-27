01457ee21926cacda542b87762949c313cae824f
Huỳnh Cung <cung.h28@gmail.com>
2025-04-27 13:52:46 +0700

    Update database.py

--- backend/app/models/database.py
+++ backend/app/models/database.py
@@ -1,7 +1,36 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field, create_engine, Session, Relationship
+from datetime import datetime
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/mydatabase")
+print(DATABASE_URL)
+
+engine = create_engine(DATABASE_URL)
+
+async def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 class User(SQLModel, table=True):
-  pass
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True, nullable=False)
+    password: str = Field(nullable=False)
 
-class Image(SQLModel, table=True):
-  pass
+    images: list["ImageMetadata"] = Relationship(back_populates="user")
+
+class LoginRequest(SQLModel):
+    username: str
+    password: str
+
+class ImageMetadata(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    filename: str = Field(nullable=False)
+    content_type: str = Field(nullable=False)
+    size: int = Field(nullable=False)
+    upload_time: datetime = Field(default_factory=datetime.now())
+    user_id: int = Field(foreign_key="user.id", nullable=False)  
+
+    user: "User" = Relationship(back_populates="images")
