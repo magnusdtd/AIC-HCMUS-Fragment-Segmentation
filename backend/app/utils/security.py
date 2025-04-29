4ce3b81d0340e4f731dd9f1ad05ea6bbf52f95c7
Huỳnh Cung <cung.h28@gmail.com>
2025-04-29 22:54:31 +0700

    Update security.py

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,6 +1,30 @@
+from fastapi_login import LoginManager
+from sqlmodel import Session, select
+from passlib.context import CryptContext
+from app.models.database import User, get_session
+from typing import Optional, Callable, Iterator
+import os
 
-def hash_password(password: str) -> str:
-  pass
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-  pass
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+
+manager = LoginManager(SECRET_KEY, token_url="/api/auth/login")
+
+@manager.user_loader(session_provider = get_session)
+def load_user(
+    username: str, 
+    db: Optional[Session] = None,
+    session_provider: Callable[[], Iterator[Session]] = None
+) -> Optional[User]:
+
+    if db is None and session_provider is None:
+        raise ValueError("db and session_provider cannot both be None.")
+
+    if db is None:
+        db = next(session_provider())
+
+    statement = select(User).where(User.username == username)
+    user = db.exec(statement).first()
+    return user
